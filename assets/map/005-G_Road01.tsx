d49560a68d9a95038e9380c4bfdf0b81871fee1c
worm <810202771@qq.com>
2020-03-25 22:46:12 +0800

    '第一次提交'

---
+++ assets/map/005-G_Road01.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="001-Grassland01" tilewidth="128" tileheight="160" tilecount="6" columns="2">
+ <image source="Tilesets/001-Grassland01.png" width="256" height="576"/>
+</tileset>
